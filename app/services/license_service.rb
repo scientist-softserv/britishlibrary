d1d812a19337c53cd3d6cdeb853e85be67def68c
Rob Kaufman <rob@notch8.com>
2021-05-11 08:55:35 -0700

    add ubiquity authorities

---
+++ app/services/license_service.rb
@@ -0,0 +1,31 @@
+# overrides hyrax/app/services/hyrax/license_service.rb
+
+module LicenseService
+  mattr_accessor :authority
+  self.authority = Qa::Authorities::Local.subauthority_for('licenses')
+
+  def self.active?(id)
+    authority.find(id).fetch('term', nil )
+  end
+
+  def self.active_elements
+    authority.all.select { |e| e.fetch('active') }
+  end
+
+  def self.label(id)
+    authority.find(id).fetch('term', '[Error: Unknown value]' )
+  end
+
+  def self.select_active_options
+    active_elements.map { |e| [e[:label], e[:id]] }
+  end
+
+  def self.include_current_value(value, _index, render_options, html_options)
+    unless value.blank? || active?(value)
+      html_options[:class] << ' force-select'
+      render_options += [[label(value), value]]
+    end
+    [render_options, html_options]
+  end
+
+end
