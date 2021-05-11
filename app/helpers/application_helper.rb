352d940b375dc2c627d4b0b8724d39443f57b18c
Benjamin Kiah Stroud <kiah@notch8.com>
2021-05-11 13:01:50 -0700

    fix institution field

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
+# TODO ~alignment: Bring over remaining methods from BL
 module ApplicationHelper
   include ::HyraxHelper
   include Hyrax::OverrideHelperBehavior
   include GroupNavigationHelper
+
+  def ubiquity_url_parser(original_url)
+    full_url = URI.parse(original_url)
+    if full_url.host.present? && full_url.host.class == String
+      full_url.host.split('.').first
+    end
+  end
 end
