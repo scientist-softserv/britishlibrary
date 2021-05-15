1dc0ba184512e29c161c9f449cfbfd9ad95dff1d
Benjamin Kiah Stroud <kiah@notch8.com>
2021-05-14 18:08:33 -0700

    helper methods for work show pages

--- app/helpers/multiple_metadata_fields_helper.rb
+++ app/helpers/multiple_metadata_fields_helper.rb
@@ -1,5 +1,11 @@
 # TODO ~alignment: Bring over remaining methods from BL
 module MultipleMetadataFieldsHelper
+  #Here we are checking in the works and search result page if the hash_keys for json fields
+  # include values for either isni or orcid before displaying parenthesis
+  def display_paren?(hash_keys, valid_keys)
+    (hash_keys & valid_keys).any?
+  end
+
   def get_model(presenter_record, model_class, field, multipart_sort_field_name = nil)
     #model ||= model_class.constantize.new
 
@@ -24,5 +30,20 @@ module MultipleMetadataFieldsHelper
     !!JSON.parse(data)  if data.class == String
     rescue JSON::ParserError
       false
+  end
+
+  private
+
+  def sort_hash(array_of_hash, key)
+    #return array_of_hash if array_of_hash.class != Array
+
+    if (key.present? && array_of_hash.first.class == Hash)
+      #allows the sort to function even if the value of a hash is nil
+      array_of_hash.sort_by{ |hash| hash[key].to_i}
+    end
+  end
+
+  def display_data_with_comma_separated(creator_hash, display_order)
+    display_order.map { |ele| creator_hash[ele].presence }.compact.join(', ')
   end
 end
