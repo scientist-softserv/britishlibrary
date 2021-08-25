c2e8be81dc0f546b295bc9da1c78d68df507c80d
summer-cook <summercook@bellsouth.net>
2021-08-25 15:12:45 -0700

    change arrows used in sort fields

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -346,10 +346,10 @@ class CatalogController < ApplicationController
     # except in the relevancy case).
     # label is key, solr field is value
     config.add_sort_field "score desc, #{uploaded_field} desc", label: "relevance"
-    config.add_sort_field "#{uploaded_field} desc", label: "date uploaded \u25BC"
-    config.add_sort_field "#{uploaded_field} asc", label: "date uploaded \u25B2"
-    config.add_sort_field "#{modified_field} desc", label: "date modified \u25BC"
-    config.add_sort_field "#{modified_field} asc", label: "date modified \u25B2"
+    config.add_sort_field "#{uploaded_field} desc", label: "date uploaded \u2193"
+    config.add_sort_field "#{uploaded_field} asc", label: "date uploaded \u2191"
+    config.add_sort_field "#{modified_field} desc", label: "date modified \u2193"
+    config.add_sort_field "#{modified_field} asc", label: "date modified \u2191"
 
     config.oai = {
       provider: {
