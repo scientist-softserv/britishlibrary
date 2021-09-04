3b1bb2f30dcb0d62c27c72401a9ed11dce8598b9
Rob Kaufman <rob@notch8.com>
2021-09-04 13:25:31 -0700

    remove depricated human readable types

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,8 +14,6 @@ class Book < ActiveFedora::Base
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
-  self.human_readable_type = 'Book'
-
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
--- app/models/time_based_media.rb
+++ app/models/time_based_media.rb
@@ -15,8 +15,6 @@ class TimeBasedMedia < ActiveFedora::Base
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
-  self.human_readable_type = 'Time Based Media'
-
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
