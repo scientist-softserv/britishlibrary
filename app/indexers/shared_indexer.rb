1451c497a3ca7444857e8a3b132ed904df5a8295
Alisha Evans <alisha@notch8.com>
2021-09-07 20:04:51 -0700

    move code out of block

--- app/indexers/shared_indexer.rb
+++ app/indexers/shared_indexer.rb
@@ -11,8 +11,8 @@ class SharedIndexer < Hyrax::WorkIndexer
       solr_doc[Solrizer.solr_name('doi_oai', :stored_searchable)] = object.doi.presence
       solr_doc["resource_type_label_ssim"] = object.resource_type.map do |rt|
         Hyrax::ResourceTypesService.label(rt)
-      solr_doc[Solrizer.solr_name('bulkrax_identifier', :facetable)] = object.bulkrax_identifier
       end
+      solr_doc[Solrizer.solr_name('bulkrax_identifier', :facetable)] = object.bulkrax_identifier
     end
   end
 end
