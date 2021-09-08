ec0f1e369e4abee8b3817fbd94ed415d8e182279
Alisha Evans <alisha@notch8.com>
2021-09-07 19:22:00 -0700

    update the bulkrax_identifier type

--- app/indexers/shared_indexer.rb
+++ app/indexers/shared_indexer.rb
@@ -9,6 +9,7 @@ class SharedIndexer < Hyrax::WorkIndexer
       solr_doc[Solrizer.solr_name('abstract_oai', :stored_searchable)] = object.abstract.presence
       solr_doc[Solrizer.solr_name('official_link_oai', :stored_searchable)] = object.official_link.presence
       solr_doc[Solrizer.solr_name('doi_oai', :stored_searchable)] = object.doi.presence
+      solr_doc[Solrizer.solr_name('bulkrax_identifier', :facetable)] = object.bulkrax_identifier
     end
   end
 end
