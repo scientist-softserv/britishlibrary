d55cf24cf5094b709455ddf50d4f12195bea3581
edward <bauchiroad@gmail.com>
2021-09-09 18:38:40 +0100

    draft of uncompleted implementation of cross tenant search

--- app/jobs/create_fcrepo_endpoint_job.rb
+++ app/jobs/create_fcrepo_endpoint_job.rb
@@ -4,6 +4,8 @@ class CreateFcrepoEndpointJob < ApplicationJob
   non_tenant_job
 
   def perform(account)
+    return NilFcrepoEndpoint.new if account.is_shared_search_enabled?
+
     name = account.tenant.parameterize
 
     account.create_fcrepo_endpoint(base_path: "/#{name}")
