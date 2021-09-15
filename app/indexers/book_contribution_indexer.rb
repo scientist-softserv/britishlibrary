1cfb39401fb37882f58f8182a00a3cb7e3603c4c
summer-cook <summercook@bellsouth.net>
2021-09-15 15:35:23 -0700

    remove duplicate code from all specific worktypes because it can stay only in the shared indexer

--- app/indexers/book_contribution_indexer.rb
+++ app/indexers/book_contribution_indexer.rb
@@ -13,7 +13,6 @@ class BookContributionIndexer < SharedIndexer
   def generate_solr_document
     super.tap do |solr_doc|
       solr_doc[Solrizer.solr_name('editor_list', :stored_searchable)] = Ubiquity::ParseJson.new(object.editor.first).fetch_value_based_on_key('editor')
-      solr_doc['year_published_isi'] = object.date_published[0...4].to_i if object.date_published.present?
     end
   end
 end
