e9c26cd877828b6b6cee6ad32bc92636835040aa
Rob Kaufman <rob@notch8.com>
2021-09-18 02:10:29 -0700

    run migration, remove any settings in bl code

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,7 +2,7 @@
 
 module AccountsHelper
   def host_for(sub_domain)
-    default_host = ENV.fetch('HYKU_DEFAULT_HOST', "%{tenant}.#{current_account.admin_host}")
+    default_host = ENV.fetch('HYKU_DEFAULT_HOST', "%{tenant}.#{Account.admin_host}")
     default_host.gsub('%{tenant}', sub_domain)
   end
 end
