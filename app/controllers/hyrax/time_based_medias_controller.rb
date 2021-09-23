5e5e10e9044b2e0fd9a71b03758ff3313afa57a7
Alisha Evans <alisha@notch8.com>
2021-09-23 11:56:07 -0700

    Inject Investigation controller hooks on 16 hyrax controllers

--- app/controllers/hyrax/time_based_medias_controller.rb
+++ app/controllers/hyrax/time_based_medias_controller.rb
@@ -3,6 +3,36 @@
 
 module Hyrax
   class TimeBasedMediasController < SharedBehaviorsController
+    # begin inject IrusAnalytics::InjectControllerHooksGenerator: include IrusAnalytics controller behavior
+    include IrusAnalytics::Controller::AnalyticsBehaviour
+    # end inject IrusAnalytics::InjectControllerHooksGenerator: include IrusAnalytics controller behavior
+    # begin inject IrusAnalytics::InjectControllerHooksGenerator: IrusAnalytics after action
+    after_action :send_irus_analytics_investigation, only: [:show]
+    # end inject IrusAnalytics::InjectControllerHooksGenerator: IrusAnalytics after action
+  public
+    # begin inject IrusAnalytics::InjectControllerHooksGenerator: item_identifier_for_irus_analytics
+    def item_identifier_for_irus_analytics
+      # return the OAI identifier
+      # http://www.openarchives.org/OAI/2.0/guidelines-oai-identifier.htm
+      CatalogController.blacklight_config.oai[:provider][:record_prefix] + ":" + params[:id]
+    end
+    # end inject IrusAnalytics::InjectControllerHooksGenerator: item_identifier_for_irus_analytics
+    # begin inject IrusAnalytics::InjectControllerHooksGenerator: skip_send_irus_analytics?
+    def skip_send_irus_analytics?(usage_event_type)
+      # return true to skip tracking, for example to skip curation_concerns.visibility == 'private'
+      case usage_event_type
+      when 'Investigation'
+        false
+      when 'Request'
+        false
+      end
+    end
+    # end inject IrusAnalytics::InjectControllerHooksGenerator: skip_send_irus_analytics?
+
+
+
+
+
     self.curation_concern_type = ::TimeBasedMedia
 
     # Use this line if you want to use a custom presenter
