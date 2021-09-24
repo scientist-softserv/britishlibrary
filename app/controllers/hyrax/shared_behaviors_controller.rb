b570b3d3cca7ec3463f83da65287c5ec9d6d0b25
Alisha Evans <alisha@notch8.com>
2021-09-24 09:49:11 -0700

    formatting + remove comments + remove downloads hook

--- app/controllers/hyrax/shared_behaviors_controller.rb
+++ app/controllers/hyrax/shared_behaviors_controller.rb
@@ -3,25 +3,17 @@ module Hyrax
     # Adds Hyrax behaviors to the controller.
     include Hyrax::WorksControllerBehavior
     include Hyrax::BreadcrumbsForWorks
-    # begin inject IrusAnalytics::InjectControllerHooksGenerator: include IrusAnalytics controller behavior
     include IrusAnalytics::Controller::AnalyticsBehaviour
-    # end inject IrusAnalytics::InjectControllerHooksGenerator: include IrusAnalytics controller behavior
-    # begin inject IrusAnalytics::InjectControllerHooksGenerator: IrusAnalytics after action
+    # use the investigation hook to track work_type views
     after_action :send_irus_analytics_investigation, only: [:show]
-    # end inject IrusAnalytics::InjectControllerHooksGenerator: IrusAnalytics after action
-    # begin inject IrusAnalytics::InjectControllerHooksGenerator: IrusAnalytics after action
-    after_action :send_irus_analytics_request, only: [:zip_download]
-    # end inject IrusAnalytics::InjectControllerHooksGenerator: IrusAnalytics after action
 
   public
-    # begin inject IrusAnalytics::InjectControllerHooksGenerator: item_identifier_for_irus_analytics
     def item_identifier_for_irus_analytics
       # return the OAI identifier
       # http://www.openarchives.org/OAI/2.0/guidelines-oai-identifier.htm
       CatalogController.blacklight_config.oai[:provider][:record_prefix] + ":" + params[:id]
     end
-    # end inject IrusAnalytics::InjectControllerHooksGenerator: item_identifier_for_irus_analytics
-    # begin inject IrusAnalytics::InjectControllerHooksGenerator: skip_send_irus_analytics?
+
     def skip_send_irus_analytics?(usage_event_type)
       # return true to skip tracking, for example to skip curation_concerns.visibility == 'private'
       case usage_event_type
@@ -31,11 +23,6 @@ module Hyrax
         false
       end
     end
-    # end inject IrusAnalytics::InjectControllerHooksGenerator: skip_send_irus_analytics?
-
-
-
-
 
     # TODO include Ubiquity::BreadcrumbOverride
 
