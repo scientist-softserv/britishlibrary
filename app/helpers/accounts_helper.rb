5534e9164f1d272b09bf7bbb23965aeb1cb72cd5
Rob Kaufman <rob@notch8.com>
2021-09-26 00:43:35 -0700

    show search on main page

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -5,4 +5,8 @@ module AccountsHelper
     default_host = ENV.fetch('HYKU_DEFAULT_HOST', "%{tenant}.#{Account.admin_host}")
     default_host.gsub('%{tenant}', sub_domain)
   end
+
+  def full_search_url
+    "//#{host_for('search')}/catalog"
+  end
 end
