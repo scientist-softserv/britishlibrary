23df939c663b626781316440d842244f53091946
Alisha Evans <alisha@notch8.com>
2021-09-27 12:24:26 -0700

    using active job instead of resque

--- config/initializers/resque.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-config = YAML.safe_load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
-Resque.redis = config.merge(thread_safe: true)
