331b3444237a0ed6b24eb5b8a92f40bc4eca2357
Alisha Evans <alisha@notch8.com>
2021-09-28 17:48:39 -0700

    add the cname indexer

--- app/indexers/shared_indexer.rb
+++ app/indexers/shared_indexer.rb
@@ -14,6 +14,7 @@ class SharedIndexer < Hyrax::WorkIndexer
       end
       solr_doc[Solrizer.solr_name('bulkrax_identifier', :facetable)] = object.bulkrax_identifier
       solr_doc['year_published_isi'] = object.date_published[0...4].to_i if object.date_published.present?
+      solr_doc[Solrizer.solr_name('account_cname')] = Site.instance.account.cname
     end
   end
 end
