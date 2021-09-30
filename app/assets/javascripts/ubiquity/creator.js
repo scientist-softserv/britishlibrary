6eb5c3e102cc88acff84df25594f3ca0de985594
Alisha Evans <alisha@notch8.com>
2021-09-30 16:19:49 -0700

    refactor, get rid of deprecated methods and add more comments

--- app/assets/javascripts/ubiquity/creator.js
+++ app/assets/javascripts/ubiquity/creator.js
@@ -16,116 +16,101 @@ $(document).on('turbolinks:load', function() {
     hiddenInput.val(parseInt(lastInputCount) + 1);
 
     // add the cloned section at the end of the creator list
-    // and set "Personal" as the type
-    $(`${ubiquityCreatorClass}`).last().after(clonedCreatorSection)
-    $('.ubiquity_creator_name_type').last().val('Personal').change()
+    // and set 'Personal' as the type
+    $(`${ubiquityCreatorClass}`).last().after(clonedCreatorSection);
+    $('.ubiquity_creator_name_type').last().val('Personal').change();
   });
 });
-
 
 // remove selected creator section
 $(document).on('turbolinks:load', function() {
   return $('body').on('click', '.remove_creator', function(event) {
     event.preventDefault();
 
-    const ubiquityCreatorClass = $(this).attr('data-removeUbiquityCreator');
-
     if ($('.ubiquity-meta-creator').length > 1 ) {
+      const ubiquityCreatorClass = $(this).attr('data-removeUbiquityCreator');
       $(this).closest(`div${ubiquityCreatorClass}`).remove();
     }
   });
 });
 
-// show either the personal or org creator fields when the creator name type is changed
+// set the initial creator name type shown on screen
 $(document).on('turbolinks:load', function() {
-  return $('body').on('change', '.ubiquity_creator_name_type', function(event) {
-    if (event.target.value == 'Personal') {
-      var _this = $(this.parentElement);
-      hideCreatorOrganization(_this);
-      _this = $(this.parentElement).siblings('.ubiquity_personal_fields')
-      creatorUpdateRequired(_this, 'family')
-      creatorUpdateRequired(_this, 'given')
-
-    } else {
-      var _this = $(this.parentElement);
-      hideCreatorPersonal(_this);
-      _this = $(this.parentElement).siblings('.ubiquity_organization_fields:last');
-      creatorUpdateRequired(_this);
-    }
+  return $('body').on('change', '.ubiquity_creator_name_type', function() {
+    displayFields(this.parentElement, this.value);
   });
 });
 
-$(document).on("turbolinks:load", function() {
+// show either the personal or org creator fields when the creator name type is changed
+$(document).on('turbolinks:load', function() {
   $('.ubiquity_creator_name_type').each(function() {
-    })
-    displayFields(this)
+    displayFields(this, this.value);
   })
 });
 
-function displayFields(self) {
-  var _this = $(self);
+function displayFields(self, value) {
+  const _this = $(self);
 
-  if (self.value == 'Personal') {
+  if (value == 'Personal') {
+    const lastPersonalSibling = _this.siblings('.ubiquity_personal_fields').last();
+
     hideCreatorOrganization(_this);
+    creatorUpdateRequired(lastPersonalSibling, 'family');
+    creatorUpdateRequired(lastPersonalSibling, 'given');
 
-    _this = $(self).siblings('.ubiquity_personal_fields:last');
-    creatorUpdateRequired(_this, 'family')
-    creatorUpdateRequired(_this, 'given')
+  } else if (value == 'Organisational') {
+    const lastOrgSibling = _this.siblings('.ubiquity_organization_fields').last();
 
-
-  } else if (self.value == 'Organisational') {
     hideCreatorPersonal(_this);
-
-    var _this = $(self).siblings('.ubiquity_organization_fields:last');
-    creatorUpdateRequired(_this, 'organization')
+    creatorUpdateRequired(lastOrgSibling, 'organization');
 
   } else {
-    $('.ubiquity_creator_name_type:last').val('Personal').change()
+    $('.ubiquity_creator_name_type').last().val('Personal').change();
   }
 }
 
-$(document).on("turbolinks:load", function() {
-  return $("body").on("blur", ".ubiquity_creator_organization_name", function (event) {
+$(document).on('turbolinks:load', function() {
+  return $('body').on('blur', '.ubiquity_creator_organization_name', function (event) {
     event.preventDefault();
 
-    var _this = $(this).parent('.ubiquity_organization_fields:last');
-    creatorUpdateRequired(_this, 'organization')
+    const _this = $(this).parent('.ubiquity_organization_fields').last();
+    creatorUpdateRequired(_this, 'organization');
   });
 });
 
-$(document).on("turbolinks:load", function() {
-  return $("body").on("blur", ".ubiquity_creator_family_name, .ubiquity_creator_given_name", function (event) {
+$(document).on('turbolinks:load', function() {
+  return $('body').on('blur', '.ubiquity_creator_family_name, .ubiquity_creator_given_name', function (event) {
     event.preventDefault();
 
-    _this = $(this).parent('.ubiquity_personal_fields:last');
-    creatorUpdateRequired(_this, 'family')
-    creatorUpdateRequired(_this, 'given')
+    const _this = $(this).parent('.ubiquity_personal_fields').last();
+    creatorUpdateRequired(_this, 'family');
+    creatorUpdateRequired(_this, 'given');
   });
 });
 
 function creatorUpdateRequired(self, field) {
-  const givenName = self.find('.ubiquity_creator_given_name:last').val();
-  const familyName = self.find('.ubiquity_creator_family_name:last').val();
+  const givenName = self.find('.ubiquity_creator_given_name').last().val();
+  const familyName = self.find('.ubiquity_creator_family_name').last().val();
 
   if (givenName || familyName) {
-    self.find(`.ubiquity_creator_${field}_name:last`).attr('required', false);
+    self.find(`.ubiquity_creator_${field}_name`).last().attr('required', false);
   } else {
-    self.find(`.ubiquity_creator_${field}_name:last`).attr('required', true);
+    self.find(`.ubiquity_creator_${field}_name`).last().attr('required', true);
   }
 }
 
 function hideCreatorOrganization(self) {
-  self.siblings(".ubiquity_personal_fields").show()
-  self.siblings(".ubiquity_organization_fields").find(".ubiquity_creator_organization_name:last").val('')
-  self.siblings(".ubiquity_organization_fields").find(".ubiquity_creator_organization_name:last").removeAttr('required');
-  self.siblings(".ubiquity_organization_fields").hide();
+  self.siblings('.ubiquity_personal_fields').show();
+  self.siblings('.ubiquity_organization_fields').find('.ubiquity_creator_organization_name').last().val('');
+  self.siblings('.ubiquity_organization_fields').find('.ubiquity_creator_organization_name').last().removeAttr('required');
+  self.siblings('.ubiquity_organization_fields').hide();
 }
 
 function hideCreatorPersonal(self) {
-  self.siblings(".ubiquity_organization_fields").show()
-  self.siblings(".ubiquity_personal_fields").find(".ubiquity_creator_family_name:last").val('').removeAttr('required');
-  self.siblings(".ubiquity_personal_fields").find(".ubiquity_creator_given_name:last").val('').removeAttr('required');
-  self.siblings(".ubiquity_personal_fields").find(".ubiquity_creator_orcid:last").val('');
-  self.siblings(".ubiquity_personal_fields").find(".ubiquity_creator_institutional_relationship:last").val('');
-  self.siblings(".ubiquity_personal_fields").hide();
+  self.siblings('.ubiquity_organization_fields').show();
+  self.siblings('.ubiquity_personal_fields').find('.ubiquity_creator_family_name').last().val('').removeAttr('required');
+  self.siblings('.ubiquity_personal_fields').find('.ubiquity_creator_given_name').last().val('').removeAttr('required');
+  self.siblings('.ubiquity_personal_fields').find('.ubiquity_creator_orcid').last().val('');
+  self.siblings('.ubiquity_personal_fields').find('.ubiquity_creator_institutional_relationship').last().val('');
+  self.siblings('.ubiquity_personal_fields').hide();
 }
