2a3349561f7c2ecea14740cf6e2872af0e75179a
summer-cook <summercook@bellsouth.net>
2021-09-30 15:43:05 -0700

    change toggle to unique classes, add show more and collapse to 2 attribute renderers

--- app/renderers/alphabetical_sort_attribute_renderer.rb
+++ app/renderers/alphabetical_sort_attribute_renderer.rb
@@ -4,13 +4,29 @@ class AlphabeticalSortAttributeRenderer < Hyrax::Renderers::AttributeRenderer
     markup = ''
 
     return markup if values.blank? && !options[:include_empty]
-    markup << %(<div class='metadata-group'><dt>#{label}</dt>\n<dd><ul class='tabular'>)
-    attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field}")
-    sorted_arr = Array(values).sort_by(&:downcase)
-    sorted_arr.each do |value|
+    markup << %(
+      <div class='metadata-group'>
+        <dt>#{label}</dt>\n
+        <dd>
+          <ul class='tabular'>)
+          sorted_arr = Array(values).sort_by(&:downcase)
+          sorted_arr.each_with_index do |value, index|
+            attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field} #{'collapse' if index > 4}")
       markup << "<li#{html_attributes(attributes)}>#{attribute_value_to_html(value.to_s)}</li>"
     end
-    markup << %(</ul></dd></div>)
+    if Array(values).length > 5
+      markup << %(
+            <li>
+              <a class="collapse-fields" data-toggle="collapse" href=".attribute-#{field}.collapse" aria-expanded="false" aria-controls="collapse">
+                <span>Show More</span>
+                <span style='display:none'>Show Less</span>
+              </a>
+            </li>)
+    end
+    markup << %(
+        </ul>
+      </dd>
+    </div>)
     markup.html_safe
   end
 end
--- app/renderers/hyrax/renderers/attribute_renderer.rb
+++ app/renderers/hyrax/renderers/attribute_renderer.rb
@@ -12,18 +12,26 @@ Hyrax::Renderers::AttributeRenderer.class_eval do
     markup = ''
 
     return markup if values.blank? && !options[:include_empty]
-    markup << %(<div class='metadata-group'><dt>#{label}</dt>\n<dd><ul class='tabular'>)
-    attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field}")
-    Array(values).each do |value|
+    markup << %(
+      <div class='metadata-group'>
+        <dt>#{label}
+        </dt>\n
+        <dd>
+          <ul class='tabular'>)
+          Array(values).each_with_index do |value, index|
+            attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field} #{'collapse' if index > 4}")
       markup << "<li#{html_attributes(attributes)}>#{attribute_value_to_html(value.to_s)}</li>"
     end
+    if Array(values).length > 5
     markup << %(
-        <li>
-          <a class="collapse-fields" data-toggle="collapse" href=".collapse" aria-expanded="false" aria-controls="collapse">
-            <span>Show More</span>
-            <span style='display:none'>Show Less</span>
-          </a>
-        </li>
+          <li>
+            <a class="collapse-fields" data-toggle="collapse" href=".attribute-#{field}.collapse" aria-expanded="false" aria-controls="collapse">
+              <span>Show More</span>
+              <span style='display:none'>Show Less</span>
+            </a>
+          </li>)
+    end
+    markup << %(
         </ul>
       </dd>
     </div>)
@@ -34,14 +42,31 @@ Hyrax::Renderers::AttributeRenderer.class_eval do
     values&.delete("")
     return '' if values.blank? && !options[:include_empty]
 
-    markup = %(<div class='metadata-group'><dt>#{label}</dt>\n<dd><ul class='tabular'>)
+    markup = %(<div class='metadata-group'>
+                <dt>#{label}</dt>\n
+                <dd>
+                  <ul class='tabular'>)
 
-    attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field}")
-
-    markup += Array(values).map do |value|
+    
+    markup += Array(values).map.with_index do |value, index|
+      attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field} #{'collapse' if index > 4}")
       "<li#{html_attributes(attributes)}>#{attribute_value_to_html(value.to_s)}</li>"
     end.join
-    markup += %(</ul></dd></div>)
+
+    if Array(values).length > 5
+      markup << %(
+            <li>
+              <a class="collapse-fields" data-toggle="collapse" href=".attribute-#{field}.collapse" aria-expanded="false" aria-controls="collapse">
+                <span>Show More</span>
+                <span style='display:none'>Show Less</span>
+              </a>
+            </li>)
+    end
+
+    markup += %(
+        </ul>
+      </dd>
+    </div>)
 
     markup.html_safe
   end
