eb55cd579ec608db81c9a81160d900f76ecef6df
summer-cook <summercook@bellsouth.net>
2021-10-01 11:02:43 -0700

    add collapse to rights statement and fix extrnal url renderer to actually render links in diffrent li's

--- app/renderers/external_url_attribute_renderer.rb
+++ app/renderers/external_url_attribute_renderer.rb
@@ -10,20 +10,33 @@ class ExternalUrlAttributeRenderer < Hyrax::Renderers::AttributeRenderer
   private
 
     def arr_of_li_values(value)
-      markup = %(<div class='metadata-group'><dt>#{label}</dt>\n<dd><ul class='tabular'>)
-      attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field}")
-      markup << "<li#{html_attributes(attributes)}>"
+      markup = %(<div class='metadata-group'>
+                  <dt>#{label}</dt>\n
+                  <dd>
+                    <ul class='tabular'>)
       sorted_arr = value.sort_by(&:downcase)
-      links = sorted_arr.map do |url|
+      links = sorted_arr.map.with_index do |url, index|
+        attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field} #{'collapse' if index > 4}")
         complete_url = "https://" + url
         if label == 'DOI'
-          auto_link(complete_url, html: { target: '_blank' })
+          final_url = auto_link(complete_url, html: { target: '_blank' })
         else
-          auto_link(url, html: { target: '_blank' })
+          final_url = link_to(url, complete_url)
         end
+        markup << "<li#{html_attributes(attributes)}>#{final_url}</li>"
       end
-      markup << links.join('<br/>')
-      markup << %(</li></ul></dd></div>)
+      if sorted_arr.length > 5
+        markup << %(
+                  <li>
+                    <a id="#{field}-collapse" class="collapse-fields" data-toggle="collapse" href=".attribute-#{field}.collapse" aria-expanded="false" aria-controls="collapse">
+                      <span>Show More</span>
+                      <span style='display:none'>Show Less</span>
+                    </a>
+                  </li>)
+      end
+      markup << %(</ul>
+              </dd>
+            </div>)
       markup.html_safe
     end
 
