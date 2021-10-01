5fc3bf2dc81e4179da83ba082d1927ad9a692b1e
summer-cook <summercook@bellsouth.net>
2021-10-01 11:36:38 -0700

    fix rubocop for these files

--- app/renderers/hyrax/renderers/attribute_renderer.rb
+++ app/renderers/hyrax/renderers/attribute_renderer.rb
@@ -10,64 +10,27 @@ Hyrax::Renderers::AttributeRenderer.class_eval do
   def render
     values&.delete("")
     markup = ''
-
     return markup if values.blank? && !options[:include_empty]
-    markup << %(
-      <div class='metadata-group'>
-        <dt>#{label}
-        </dt>\n
-        <dd>
-          <ul class='tabular'>)
-          Array(values).each_with_index do |value, index|
-            attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field} #{'collapse' if index > 4}")
+    markup << %(<div class='metadata-group'><dt>#{label}</dt>\n<dd><ul class='tabular'>)
+    Array(values).each_with_index do |value, index|
+      attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field} #{'collapse' if index > 4}")
       markup << "<li#{html_attributes(attributes)}>#{attribute_value_to_html(value.to_s)}</li>"
     end
-    if Array(values).length > 5
-    markup << %(
-          <li>
-            <a id="#{field}-collapse" class="collapse-fields" data-toggle="collapse" href=".attribute-#{field}.collapse" aria-expanded="false" aria-controls="collapse">
-              <span>Show More</span>
-              <span style='display:none'>Show Less</span>
-            </a>
-          </li>)
-    end
-    markup << %(
-        </ul>
-      </dd>
-    </div>)
+    markup << %(<li><a id="#{field}-collapse" class="collapse-fields" data-toggle="collapse" href=".attribute-#{field}.collapse" aria-expanded="false" aria-controls="collapse"><span>Show More</span><span style='display:none'>Show Less</span></a></li>) if Array(values).length > 5
+    markup << %(</ul></dd></div>)
     markup.html_safe
   end
 
   def render_dl_row
     values&.delete("")
     return '' if values.blank? && !options[:include_empty]
-
-    markup = %(<div class='metadata-group'>
-                <dt>#{label}</dt>\n
-                <dd>
-                  <ul class='tabular'>)
-
-    
+    markup = %(<div class='metadata-group'><dt>#{label}</dt>\n<dd><ul class='tabular'>)
     markup += Array(values).map.with_index do |value, index|
       attributes = microdata_object_attributes(field).merge(class: "attribute attribute-#{field} #{'collapse' if index > 4}")
       "<li#{html_attributes(attributes)}>#{attribute_value_to_html(value.to_s)}</li>"
     end.join
-
-    if Array(values).length > 5
-      markup << %(
-            <li>
-              <a id="#{field}-collapse" class="collapse-fields" data-toggle="collapse" href=".attribute-#{field}.collapse" aria-expanded="false" aria-controls="collapse">
-                <span>Show More</span>
-                <span style='display:none'>Show Less</span>
-              </a>
-            </li>)
-    end
-
-    markup += %(
-        </ul>
-      </dd>
-    </div>)
-
+    markup << %(<li><a id="#{field}-collapse" class="collapse-fields" data-toggle="collapse" href=".attribute-#{field}.collapse" aria-expanded="false" aria-controls="collapse"><span>Show More</span><span style='display:none'>Show Less</span></a></li>) if Array(values).length > 5
+    markup += %(</ul></dd></div>)
     markup.html_safe
   end
 
