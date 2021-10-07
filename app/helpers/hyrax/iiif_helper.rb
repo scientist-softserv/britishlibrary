037a75d99c0912cf38642339cb4a7ade308d66c9
Alisha Evans <alisha@notch8.com>
2021-10-07 15:56:50 +0000

    update comment

--- app/helpers/hyrax/iiif_helper.rb
+++ app/helpers/hyrax/iiif_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 # OVERRIDE of Hyrax 2.9.6 from iiif_helper.rb in the main branch as of 10/06/2021 (commit 8c688a8)
+# The `universal_viewer_base_url` and `universal_viewer_config_url` method names were needed,
+# but the string inside each is a mix of the code from main, and the current uv src
+
 module Hyrax
   module IiifHelper
     def iiif_viewer_display(work_presenter, locals = {})
