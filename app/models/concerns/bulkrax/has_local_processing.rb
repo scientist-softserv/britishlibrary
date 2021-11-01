946c902ace1e8a8af7fcf7d8ef0edff2d93008f3
Rob Kaufman <rob@notch8.com>
2021-11-01 16:49:25 -0700

    use local processing for person institutional relationship handling

--- app/models/concerns/bulkrax/has_local_processing.rb
+++ app/models/concerns/bulkrax/has_local_processing.rb
@@ -10,5 +10,23 @@ module Bulkrax::HasLocalProcessing
     ['funder', 'creator', 'contributor', 'editor', 'alternate_identifier', 'related_identifier', 'current_he_institution'].each do |key|
       parsed_metadata[key] = [parsed_metadata[key].to_json] if parsed_metadata[key].present?
     end
+
+    set_institutional_relationship
+  end
+
+  def set_institutional_relationship
+    acceptable_values = {
+      'researchassociate': 'Research associate',
+      'staffmember': 'Staff member'
+    }
+
+    ['contributor_researchassociate', 'contributor_staffmember', 'creator_researchassociate', 'creator_staffmember', 'editor_researchassociate', 'editor_staffmember'].each do |field|
+      if parsed_metadata[field].present?
+        field_part, value_part = field.split('_')
+        field_name = "#{field_part}_institutional_relationship"
+        parsed_metadata[field_name] = acceptable_values[value_part]
+        parsed_metadata.delete(field)
+      end
+    end
   end
 end
