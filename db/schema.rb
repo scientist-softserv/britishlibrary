1d4853591957b0ece473dd06f2690ad61aa179b0
LEARN Academy <learnacademy@LEARNs-Air.lan>
2021-12-14 16:53:50 -0800

    Hover-Trouble-wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_065530) do
+ActiveRecord::Schema.define(version: 2021_10_04_170708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,7 +141,7 @@ ActiveRecord::Schema.define(version: 2021_09_20_065530) do
   create_table "bulkrax_statuses", force: :cascade do |t|
     t.string "status_message"
     t.string "error_class"
-    t.string "error_message"
+    t.text "error_message"
     t.text "error_backtrace"
     t.integer "statusable_id"
     t.string "statusable_type"
