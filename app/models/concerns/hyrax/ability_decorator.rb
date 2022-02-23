fad46e5f862dd6f826f39f9403c7bad45db561fd
summer-cook <summercook@bellsouth.net>
2022-02-23 13:05:11 -0800

    remove ability logic from decorator since its in the model

--- app/models/concerns/hyrax/ability_decorator.rb
+++ app/models/concerns/hyrax/ability_decorator.rb
@@ -1,27 +1,5 @@
 module Hyrax
   module AbilityDecorator
-    def self.ability_logic
-      %i[admin_permissions
-         curation_concerns_permissions
-         operation_abilities
-         add_to_collection
-         user_abilities
-         featured_work_abilities
-         featured_collection_abilities
-         editor_abilities
-         stats_abilities
-         citation_abilities
-         proxy_deposit_abilities
-         uploaded_file_abilities
-         feature_abilities
-         admin_set_abilities
-         collection_abilities
-         collection_type_abilities
-         permission_template_abilities
-         solr_document_abilities
-         trophy_abilities]
-    end
-
     def featured_collection_abilities
       can [:create, :destroy, :update], FeaturedCollection if admin?
     end
