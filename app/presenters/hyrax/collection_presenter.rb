ac2d7abd85f39a7bc19359330254bafa03a0d37c
summer-cook <summercook@bellsouth.net>
2022-02-23 08:43:47 -0800

    fix rubocops on collection presenter

--- app/presenters/hyrax/collection_presenter.rb
+++ app/presenters/hyrax/collection_presenter.rb
@@ -3,28 +3,27 @@
 require_dependency Hyrax::Engine.root.join('app', 'presenters', 'hyrax', 'collection_presenter').to_s
 
 Hyrax::CollectionPresenter.class_eval do
-    
-    # Begin Featured Collections Methods
-    def collection_featurable?
-      user_can_feature_collection? && solr_document.public?
-    end
+  # Begin Featured Collections Methods
+  def collection_featurable?
+    user_can_feature_collection? && solr_document.public?
+  end
 
-    def display_feature_collection_link?
-      collection_featurable? && FeaturedCollection.can_create_another? && !collection_featured?
-    end
+  def display_feature_collection_link?
+    collection_featurable? && FeaturedCollection.can_create_another? && !collection_featured?
+  end
 
-    def display_unfeature_collection_link?
-      collection_featurable? && collection_featured?
-    end
+  def display_unfeature_collection_link?
+    collection_featurable? && collection_featured?
+  end
 
-    def collection_featured?
-      # only look this up if it's not boolean; ||= won't work here
-      @collection_featured = FeaturedCollection.where(collection_id: solr_document.id).exists? if @collection_featured.nil?
-      @collection_featured
-    end
+  def collection_featured?
+    # only look this up if it's not boolean; ||= won't work here
+    @collection_featured = FeaturedCollection.where(collection_id: solr_document.id).exists? if @collection_featured.nil?
+    @collection_featured
+  end
 
-    def user_can_feature_collection?
-      current_ability.can?(:create, FeaturedCollection)
-    end
-    # End Featured Collections Methods
+  def user_can_feature_collection?
+    current_ability.can?(:create, FeaturedCollection)
+  end
+  # End Featured Collections Methods
 end
