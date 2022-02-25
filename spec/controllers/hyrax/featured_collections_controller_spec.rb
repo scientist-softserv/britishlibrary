0493ac445f9d7e1f262350d4b30f2f5c917a7e37
summer-cook <summercook@bellsouth.net>
2022-02-25 15:39:06 -0800

    fix all the last rubocop errors

--- spec/controllers/hyrax/featured_collections_controller_spec.rb
+++ spec/controllers/hyrax/featured_collections_controller_spec.rb
@@ -2,14 +2,14 @@ RSpec.describe Hyrax::FeaturedCollectionsController, type: :controller do
   describe "#create" do
     before do
       sign_in create(:user)
-      expect(controller).to receive(:authorize!).with(:create, FeaturedCollection).and_return(true)
     end
 
     context "when there are no featured collections" do
       it "creates one" do
+        expect(controller).to receive(:authorize!).with(:create, FeaturedCollection).and_return(true)
         expect do
           post :create, params: { id: '1234abcd', format: :json }
-        end.to change { FeaturedCollection.count }.by(1)
+        end.to change(FeaturedCollection, :count).by(1)
         expect(response).to be_successful
       end
     end
@@ -21,9 +21,10 @@ RSpec.describe Hyrax::FeaturedCollectionsController, type: :controller do
         end
       end
       it "does not create another" do
+        expect(controller).to receive(:authorize!).with(:create, FeaturedCollection).and_return(true)
         expect do
           post :create, params: { id: '1234abcd', format: :json }
-        end.not_to change { FeaturedCollection.count }
+        end.not_to change(FeaturedCollection, :count)
         expect(response.status).to eq 422
       end
     end
@@ -32,22 +33,23 @@ RSpec.describe Hyrax::FeaturedCollectionsController, type: :controller do
   describe "#destroy" do
     before do
       sign_in create(:user)
-      expect(controller).to receive(:authorize!).with(:destroy, FeaturedCollection).and_return(true)
     end
 
     context "when the collection exists" do
       before { create(:featured_collection, collection_id: '1234abcd') }
 
       it "removes it" do
+        expect(controller).to receive(:authorize!).with(:destroy, FeaturedCollection).and_return(true)
         expect do
           delete :destroy, params: { id: '1234abcd', format: :json }
-        end.to change { FeaturedCollection.count }.by(-1)
+        end.to change(FeaturedCollection, :count).by(-1)
         expect(response.status).to eq 204
       end
     end
 
     context "when it was already removed" do
       it "doesn't raise an error" do
+        expect(controller).to receive(:authorize!).with(:destroy, FeaturedCollection).and_return(true)
         delete :destroy, params: { id: '1234abcd', format: :json }
         expect(response.status).to eq 204
       end
