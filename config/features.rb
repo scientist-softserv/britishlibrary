b59290e76686c2cd8050969fe27fcc397364b838
Shana <shana.lavina.moore@gmail.com>
2022-03-03 16:56:28 -0800

    disable share_button feature flag due to removal from homepage

--- config/features.rb
+++ config/features.rb
@@ -4,9 +4,10 @@ Flipflop.configure do
           default: true,
           description: "Shows the Featured Researcher tab on the homepage."
 
-  feature :show_share_button,
-          default: true,
-          description: "Shows the 'Share Your Work' button on the homepage."
+  # Disabling due to overriden homepage 
+  # feature :show_share_button,
+  #         default: true,
+  #         description: "Shows the 'Share Your Work' button on the homepage."
 
   feature :show_featured_collections,
           default: true,
