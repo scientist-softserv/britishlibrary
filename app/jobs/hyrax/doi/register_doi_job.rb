4f0c2ec2f956f6a93a24a95933c5deb6adbade20
Rob Kaufman <rob@notch8.com>
2022-03-11 16:46:12 -0800

    stop retrying doi jobs that will never pass

--- app/jobs/hyrax/doi/register_doi_job.rb
+++ app/jobs/hyrax/doi/register_doi_job.rb
@@ -17,16 +17,16 @@ module Hyrax
         Hyrax::Identifier::Dispatcher
           .for(registrar.to_sym, **registrar_opts)
           .assign_for!(object: model, attribute: :doi)
-      rescue Hyrax::DOI::DataCiteClient::Error => e
+      rescue => e
         user = ::User.find_by(email: model.depositor) if model.depositor
 
-        if user
-          Hyrax::MessengerService.deliver(::User.audit_user,
-                                          user,
-                                          action(model, e),
-                                          "DOI failed to mint or update")
-        end
-        raise
+        Hyrax::MessengerService.deliver(::User.audit_user,
+          user,
+          action(model, e),
+          "DOI failed to mint or update") if user
+        Raven.capture_exception(e)
+        # Requeue if this is a datacite connection issue
+        raise if e.is_a? Hyrax::DOI::DataCiteClient::Error
       end
 
       def action(model, e)
