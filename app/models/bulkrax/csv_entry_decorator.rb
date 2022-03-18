73437a7f2f2c0a2036b6df87addf2a69b4794338
Shana <shana.lavina.moore@gmail.com>
2022-03-18 11:12:04 -0700

    temp disable of file to debug gitlab ci

--- app/models/bulkrax/csv_entry_decorator.rb
+++ app/models/bulkrax/csv_entry_decorator.rb
@@ -1,13 +1,13 @@
 # OVERRIDE Bulkrax 1.0.2 allow collections to happen
 
-module Bulkrax
-  module CsvEntryDecorator
-    def find_collection(collection_identifier)
-      Collection.find(collection_identifier)
-    rescue ActiveFedora::ObjectNotFoundError
-      nil
-    end
-  end
-end
+# module Bulkrax
+#   module CsvEntryDecorator
+#     def find_collection(collection_identifier)
+#       Collection.find(collection_identifier)
+#     rescue ActiveFedora::ObjectNotFoundError
+#       nil
+#     end
+#   end
+# end
 
-::Bulkrax::CsvEntry.prepend(Bulkrax::CsvEntryDecorator)
+# ::Bulkrax::CsvEntry.prepend(Bulkrax::CsvEntryDecorator)
