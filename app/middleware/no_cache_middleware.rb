9f469981b543e8c2a017232d743ec64047826769
summer-cook <summercook@bellsouth.net>
2022-03-22 14:15:15 -0700

    fix rubocop issues

--- app/middleware/no_cache_middleware.rb
+++ app/middleware/no_cache_middleware.rb
@@ -1,18 +1,17 @@
 # Taken from https://codeburst.io/service-workers-rails-middleware-841d0194144d
-#
 class NoCacheMiddleware
-  # We’ll pass 'service_workers' when we register this middleware.
+  # pass controllers when we register this middleware.
   def initialize(app, controllers)
     @app = app
     @controllers = controllers
   end
 
   def call(env)
-    # Let the next middleware classes & app do their thing first…
+    # Let the next middleware classes & app do their thing first...
     status, headers, response = @app.call(env)
     dont_cache = @controllers.any? { |controller_regex| env['REQUEST_PATH'] =~ controller_regex }
 
-    # …and modify the response if a service worker was fetched.
+    # and modify the response if a controller was fetched.
     if dont_cache
       headers["Cache-Control"] = "no-cache, no-store, must-revalidate" # HTTP 1.1.
       headers["Pragma"] = "no-cache" # HTTP 1.0.
