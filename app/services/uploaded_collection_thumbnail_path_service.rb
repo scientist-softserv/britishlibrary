89ac9995ca70e78df8ec45bd800b7c88c8f701f9
summer-cook <summercook@bellsouth.net>
2022-03-22 16:15:05 -0700

    caching fix again

--- app/services/uploaded_collection_thumbnail_path_service.rb
+++ app/services/uploaded_collection_thumbnail_path_service.rb
@@ -7,11 +7,11 @@ class UploadedCollectionThumbnailPathService < Hyrax::ThumbnailPathService
 
     # rubocop:disable Rails/FilePath, Lint/StringConversionInInterpolation
     def uploaded_thumbnail?(collection)
-      File.exist?("#{Rails.root.to_s}/public/uploaded_collection_thumbnails/#{collection.id}/#{collection.id}_card.jpg")
+      File.exist?("#{Rails.root.to_s}/public/uploads/uploaded_collection_thumbnails/#{collection.id}/#{collection.id}_card.jpg")
     end
 
     def upload_dir(collection)
-      "#{Rails.root.to_s}/public/uploaded_collection_thumbnails/#{collection.id}"
+      "#{Rails.root.to_s}/public/uploads/uploaded_collection_thumbnails/#{collection.id}"
     end
     # rubocop:enable Rails/FilePath, Lint/StringConversionInInterpolation
   end
