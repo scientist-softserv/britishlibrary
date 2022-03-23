a412fa740e640c83f254c04ee7f3b4b0a1ceb5a6
Alisha Evans <alisha@notch8.com>
2022-03-23 18:28:28 -0500

    return ability to save standard collection terms

--- app/forms/hyrax/collection_form.rb
+++ app/forms/hyrax/collection_form.rb
@@ -3,5 +3,11 @@
 module Hyrax
   class CollectionForm < Hyrax::Forms::CollectionForm
     include ::Ubiquity::AllFormsSharedBehaviour
+
+    # return the terms from Hyrax::Forms::CollectionForm that are removed in ::Ubiquity::AllFormsSharedBehaviour
+    self.terms += [:resource_type, :title, :creator, :contributor, :description,
+      :keyword, :license, :publisher, :date_created, :subject, :language,
+      :representative_id, :thumbnail_id, :identifier, :based_near,
+      :related_url, :visibility, :collection_type_gid]
   end
 end
