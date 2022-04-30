ad193bded0ae5795f9f0a99c01eb4a1e0f4df652
April Rieger <aprilrieger@Aprils-MacBook-Pro.local>
2022-04-29 17:09:33 -0700

    adds pagination to the proprietor accoutn show page tables

--- app/controllers/proprietor/accounts_controller.rb
+++ app/controllers/proprietor/accounts_controller.rb
@@ -20,6 +20,14 @@ module Proprietor
       add_breadcrumb t(:'hyrax.controls.home'), root_path
       add_breadcrumb t(:'hyrax.admin.sidebar.accounts'), proprietor_accounts_path
       add_breadcrumb @account.tenant, edit_proprietor_account_path(@account)
+
+      current_superadmin = []
+      @account.admin_emails.each do |email|
+        current_superadmin << User.find_by(email: email)
+      end
+
+      @paginatable_current_superadmin_array = Kaminari.paginate_array(current_superadmin).page(params[:user_superadmin_page]).per(5)
+      @current_nonadmin_user = User.all.page(params[:user_page]).per(5)
     end
 
     # GET /accounts/new
