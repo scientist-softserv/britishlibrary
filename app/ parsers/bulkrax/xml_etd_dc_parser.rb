8a9a606208c18205bbeda3f29c4ddd3b2e70d85b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-01-16 15:24:26 -0500

    Tidying up parser to leverage inheritance
    Prior to this commit, the `Bulkrax::XmlEtdDcParser` was implementing
    methods that should have been on it's parent class and/or in the
    `Bulkrax::ApplicationParser`.
    
    This change normalizes the declared methods to better mimic what's
    upstream.
    
    In particular the `Bulkrax::ApplicationParser#create_objects` (see
    below and [the code][1]).
    
    ```ruby
    def create_objects(types = [])
      types.each do |object_type|
        parser.send("create_#{object_type.pluralize}")
      end
    end
    ```
    
    Related to:
    
    - https://github.com/scientist-softserv/britishlibrary/issues/174
    - https://github.com/scientist-softserv/britishlibrary/issues/177
    - https://github.com/scientist-softserv/britishlibrary/issues/178
    - https://github.com/scientist-softserv/britishlibrary/issues/179
    - https://github.com/scientist-softserv/britishlibrary/issues/180
    - https://github.com/scientist-softserv/britishlibrary/issues/181
    - https://github.com/scientist-softserv/britishlibrary/issues/182
    - https://github.com/scientist-softserv/britishlibrary/issues/183
    - https://github.com/scientist-softserv/britishlibrary/issues/184
    - https://github.com/scientist-softserv/britishlibrary/issues/185
    - https://github.com/scientist-softserv/britishlibrary/issues/186
    - https://github.com/scientist-softserv/britishlibrary/issues/187
    
    [1]: https://github.com/samvera-labs/bulkrax/blob/2ef3577555f1ed56e6f8db7b1fac9212ea5bea7f/app/parsers/bulkrax/application_parser.rb#L159-L164

--- app/ parsers/bulkrax/xml_etd_dc_parser.rb
+++ app/ parsers/bulkrax/xml_etd_dc_parser.rb
@@ -6,28 +6,11 @@ module Bulkrax
       Bulkrax::XmlEtdDcEntry
     end
 
-    # @this not yet supported yet in bulkrax
+    # @note this not yet supported yet in bulkrax
     def file_set_entry_class; end
-
-    def create_file_sets
-      create_objects(['file_set'])
-    end
 
     def create_relationships
       ScheduleRelationshipsJob.set(wait: 5.minutes).perform_later(importer_id: importerexporter.id)
-    end
-
-    # needed to implement a passthrough as the xml parser doesn't support this yet in bulkrax.
-    def create_objects(type_array)
-      if type_array.include? 'collection'
-        create_works
-      end
-      if type_array.include? 'work'
-        create_collections
-      end
-      if type_array.include? 'relationship'
-        create_relationships
-      end
     end
   end
 end
