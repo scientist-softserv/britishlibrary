27097558635582c4d304bc9465b71c58e7d0cfcf
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-02-10 16:21:30 -0500

    Adding spec that demonstrates the bug
    You'll likely want to leverage the Bulkrax::CsvEnryDecorator to aid in
    debugging.  The method that's troublesome is [here][1].  You can copy
    that method into the decorator and debug locally.
    
    You might consider copying the entire set of instance methods from the
    `Bulkrax::HasMatchers` module while you're next debugging.
    
    Related to:
    
    - https://github.com/samvera-labs/bulkrax/issues/735
    - https://github.com/scientist-softserv/britishlibrary/issues/275
    
    [1]: https://github.com/samvera-labs/bulkrax/blob/5ba218cef3fb21179940cabd3e9a9136b769a692/app/models/concerns/bulkrax/has_matchers.rb#L66-L84

--- spec/models/bulkrax/csv_entry_spec.rb
+++ spec/models/bulkrax/csv_entry_spec.rb
@@ -20,8 +20,8 @@ module Bulkrax
     # @todo This could replace some of the factories in Bulkrax's spec suite.
     # @note This presently assumes as CSV oriented format; or "We haven't checked this against CSV".
     #       And the signature of this method should be considered volatile.
-    def self.build_csv_entry_for(identifier:, data:, parser_class_name:, entry_class:)
-      import_file_path = Rails.root.join("spec", "fixtures", "csv", "british-library.csv")
+    def self.build_csv_entry_for(identifier:, data:, parser_class_name:, entry_class:, **kwargs)
+      import_file_path = kwargs.fetch(:import_file_path)
       importer = Bulkrax::Importer.new(
         name: "Test importer for identifier #{identifier}",
         admin_set_id: "admin_set/default",
@@ -48,24 +48,47 @@ RSpec.describe Bulkrax::CsvEntry do
         data: data,
         identifier: identifier,
         parser_class_name: 'Bulkrax::CsvParser',
-        entry_class: described_class
+        entry_class: described_class,
+        import_file_path: import_file_path
       )
     end
 
-    let(:identifier) { 'bl-26-0' }
-    let(:data) do
-      {
-        work_type: "ThesisOrDissertation",
-        title: "Thesis",
-        bulkrax_identifier: identifier,
-        access_rights: "true"
-      }
+    context 'basic parser test' do
+      let(:import_file_path) { Rails.root.join("spec", "fixtures", "csv", "british-library.csv") }
+      let(:identifier) { 'bl-26-0' }
+      let(:data) do
+        {
+          work_type: "ThesisOrDissertation",
+          title: "Thesis",
+          bulkrax_identifier: identifier,
+          access_rights: "true"
+        }
+      end
+
+      it "assigns factory_class and parsed_metadata" do
+        entry.build_metadata
+        expect(entry.factory_class).to eq(ThesisOrDissertation)
+        expect(entry.parsed_metadata.fetch("ethos_access_rights")).to eq(["true"])
+      end
     end
 
-    it "assigns factory_class and parsed_metadata" do
-      entry.build_metadata
-      expect(entry.factory_class).to eq(ThesisOrDissertation)
-      expect(entry.parsed_metadata.fetch("ethos_access_rights")).to eq(["true"])
+    context 'with production metadata (see https://github.com/scientist-softserv/britishlibrary/issues/275)' do
+      let(:import_file_path) { Rails.root.join("spec", "fixtures", "csv", "BlackpoolGazetteHerald.csv") }
+      let(:identifier) { '123' }
+      let(:data) do
+        # We're using the fixture CSV for data.
+        Bulkrax::CsvEntry.read_data(import_file_path).first.to_h
+      end
+
+      it "builds the metadata without exception" do
+        # Note the `current_he_institution` column maps to `current_he_institution_name` which is
+        # part of a complex bulkrax object that is represented as the property.
+        #
+        # My suspicion is that we're first adding `current_he_institution` as `""` to the parsed
+        # metadata then attempting to handle the complex object.
+        entry.build_metadata
+        expect(entry.factory_class).to eq(Dataset)
+      end
     end
   end
 end
