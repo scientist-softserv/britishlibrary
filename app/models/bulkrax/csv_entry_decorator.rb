2f92adf78b533fb7715b88664542a8b217142952
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-03-08 09:09:58 -0500

    Bumping Bulkrax and removing unneeded decorators
    The Bulkrax update provides the following fixes:
    
    - Avoid building value/object if hyrax_record does not respond
    - Fixing bug regarding total and export limits
    
    As such, some of the overrides are no longer needed.
    
    Related to:
    
    - https://github.com/samvera-labs/bulkrax/pull/756

--- app/models/bulkrax/csv_entry_decorator.rb
+++ app/models/bulkrax/csv_entry_decorator.rb
@@ -1,20 +1,6 @@
 module Bulkrax
   # Override Bulkrax v5.1.0
   module CsvEntryDecorator
-    # NOTE: Remove this once https://github.com/samvera-labs/bulkrax/pull/756 is merged
-    def build_object(key, value)
-      return unless hyrax_record.respond_to?(value['object'])
-
-      super(key, value)
-    end
-
-    # NOTE: Remove this once https://github.com/samvera-labs/bulkrax/pull/756 is merged
-    def build_value(key, value)
-      return unless hyrax_record.respond_to?(key.to_s)
-
-      super(key, value)
-    end
-
     # Without this hack, when we export a FileSet, it skips exporting the FileSet's creator.  This
     # is because we are overloading the `creator` in the parser.  With this change, the complex
     # `creator` object for works will export correctly (e.g. have the constituent field parts in the
@@ -36,45 +22,3 @@ module Bulkrax
   end
 end
 Bulkrax::CsvEntry.prepend(Bulkrax::CsvEntryDecorator)
-
-# NOTE: Remove this module once https://github.com/samvera-labs/bulkrax/pull/756 is merged
-module CsvParserDecorator
-  def total
-    @total =
-      if importer?
-        importer.parser_fields['total'] || 0
-      elsif exporter?
-        limit.to_i.zero? ? current_records_for_export.count : limit.to_i
-      else
-        0
-      end
-
-    return @total
-  rescue StandardError
-    @total = 0
-  end
-
-  def write_files
-    require 'open-uri'
-    folder_count = 0
-    # TODO: This is not performant as well; unclear how to address, but lower priority as of
-    #       <2023-02-21 Tue>.
-    sorted_entries = sort_entries(importerexporter.entries.uniq(&:identifier))
-                     .select { |e| valid_entry_types.include?(e.type) }
-
-    group_size = limit.to_i.zero? ? total : limit.to_i
-    sorted_entries[0..group_size].in_groups_of(records_split_count, false) do |group|
-      folder_count += 1
-
-      CSV.open(setup_export_file(folder_count), "w", headers: export_headers, write_headers: true) do |csv|
-        group.each do |entry|
-          csv << entry.parsed_metadata
-          next if importerexporter.metadata_only? || entry.type == 'Bulkrax::CsvCollectionEntry'
-
-          store_files(entry.identifier, folder_count.to_s)
-        end
-      end
-    end
-  end
-end
-Bulkrax::CsvParser.prepend(CsvParserDecorator)
