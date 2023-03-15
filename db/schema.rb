d6a15c4ffcabbbf9f1729008838dfb3ab82a949d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-03-15 09:44:48 -0400

    Adding IIIF Print schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_180915) do
+ActiveRecord::Schema.define(version: 2023_03_08_221006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,8 @@ ActiveRecord::Schema.define(version: 2022_04_13_180915) do
     t.integer "total_file_set_entries", default: 0
     t.integer "processed_works", default: 0
     t.integer "failed_works", default: 0
+    t.integer "processed_children", default: 0
+    t.integer "failed_children", default: 0
     t.index ["importer_id"], name: "index_bulkrax_importer_runs_on_importer_id"
   end
 
@@ -146,13 +148,13 @@ ActiveRecord::Schema.define(version: 2022_04_13_180915) do
   end
 
   create_table "bulkrax_pending_relationships", force: :cascade do |t|
-    t.bigint "bulkrax_importer_run_id", null: false
+    t.bigint "importer_run_id", null: false
     t.string "parent_id", null: false
     t.string "child_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "order", default: 0
-    t.index ["bulkrax_importer_run_id"], name: "index_bulkrax_pending_relationships_on_bulkrax_importer_run_id"
+    t.index ["importer_run_id"], name: "index_bulkrax_pending_relationships_on_importer_run_id"
   end
 
   create_table "bulkrax_statuses", force: :cascade do |t|
@@ -333,6 +335,32 @@ ActiveRecord::Schema.define(version: 2022_04_13_180915) do
     t.boolean "enabled", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "iiif_print_derivative_attachments", id: :serial, force: :cascade do |t|
+    t.string "fileset_id"
+    t.string "path"
+    t.string "destination_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fileset_id"], name: "index_iiif_print_derivative_attachments_on_fileset_id"
+  end
+
+  create_table "iiif_print_ingest_file_relations", id: :serial, force: :cascade do |t|
+    t.string "file_path"
+    t.string "derivative_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["file_path"], name: "index_iiif_print_ingest_file_relations_on_file_path"
+  end
+
+  create_table "iiif_print_pending_relationships", force: :cascade do |t|
+    t.string "child_title", null: false
+    t.string "parent_id", null: false
+    t.string "child_order", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parent_id"], name: "index_iiif_print_pending_relationships_on_parent_id"
   end
 
   create_table "job_io_wrappers", id: :serial, force: :cascade do |t|
@@ -818,7 +846,7 @@ ActiveRecord::Schema.define(version: 2022_04_13_180915) do
   add_foreign_key "accounts", "endpoints", column: "solr_endpoint_id", on_delete: :nullify
   add_foreign_key "bulkrax_exporter_runs", "bulkrax_exporters", column: "exporter_id"
   add_foreign_key "bulkrax_importer_runs", "bulkrax_importers", column: "importer_id"
-  add_foreign_key "bulkrax_pending_relationships", "bulkrax_importer_runs"
+  add_foreign_key "bulkrax_pending_relationships", "bulkrax_importer_runs", column: "importer_run_id"
   add_foreign_key "collection_type_participants", "hyrax_collection_types"
   add_foreign_key "content_blocks", "sites"
   add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", column: "conversation_id", name: "mb_opt_outs_on_conversations_id"
