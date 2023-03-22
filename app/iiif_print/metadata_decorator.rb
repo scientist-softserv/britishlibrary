fa0a8cd293955466f75d8f33d46bf24eed9decc0
Kirk Wang <kirk.wang@scientist.com>
2023-03-22 12:42:14 -0700

    Add handling of the other special fields
    This commit will handle the other specials fields (funder, alternative
    identifier, and related identifier) to display in the UV like it does in
    the shows page.

--- app/iiif_print/metadata_decorator.rb
+++ app/iiif_print/metadata_decorator.rb
@@ -2,30 +2,91 @@ module IiifPrint
   module MetadataDecorator
     private
 
-    # BL has three special name fields that are split into
-    # family name, given name, and also organization name
-    # which need to be handled differently
-    SPECIAL_NAME_FIELDS = [:creator, :contributor, :editor].freeze
+      # BL has three special name fields that are split into
+      # family name, given name, and also organization name
+      # which need to be handled differently
+      SPECIAL_NAME_FIELDS = [:creator, :contributor, :editor].freeze
 
-    def values_for(field_name:)
-      field_name = field_name.try(:name) || field_name
-      values = Array(work["#{field_name}_tesim"] || work["#{field_name}_dtsi"]&.to_date.try(:to_formatted_s, :standard))
-      return if values.empty?
+      def values_for(field_name:)
+        field_name = field_name.try(:name) || field_name
+        values = Array(work["#{field_name}_tesim"] || work["#{field_name}_dtsi"]&.to_date.try(:to_formatted_s, :standard))
+        return if values.empty?
 
-      SPECIAL_NAME_FIELDS.include?(field_name) ? handle_names(field_name, values) : values
-    end
-
-    def handle_names(field_name, values)
-      field = field_name.to_s
-      names = JSON.parse(values.first)
-      names.map do |hash|
-        family_name = hash["#{field}_family_name"]
-        given_name = hash["#{field}_given_name"]
-        organization_name = hash["#{field}_organization_name"]
-
-        [family_name, given_name, organization_name].compact.join(', ')
+        if SPECIAL_NAME_FIELDS.include?(field_name)
+          handle_names(field_name, values)
+        elsif field_name == :funder
+          handle_funder(values)
+        elsif field_name == :alternate_identifier
+          handle_alternative_identifier(values)
+        elsif field_name == :related_identifier
+          handle_related_identifier(values)
+        else
+          values
+        end
       end
-    end
+
+      def handle_names(field_name, values)
+        field = field_name.to_s
+        names = JSON.parse(values.first)
+        names.map do |hash|
+          family_name_key = "#{field}_family_name"
+          given_name_key = "#{field}_given_name"
+          organization_name_key = "#{field}_organization_name"
+
+          family_name = hash[family_name_key]
+          given_name = hash[given_name_key]
+          organization_name = hash[organization_name_key]
+
+          [family_name, given_name, organization_name].compact.join(', ')
+        end
+      end
+
+      def handle_funder(values)
+        hashes = JSON.parse(values.first)
+
+        result = []
+        hashes.each do |hash|
+          funder_name = hash['funder_name']
+          funder_awards = hash['funder_award'].join(' | ')
+          result << apply_label('Name', funder_name)
+          result << apply_label('Awards', funder_awards)
+        end
+
+        result
+      end
+
+      def handle_alternative_identifier(values)
+        hashes = JSON.parse(values.first)
+
+        build_identifier_values(hashes)
+      end
+
+      def handle_related_identifier(values)
+        hashes = JSON.parse(values.first)
+
+        # Replace "relation_type" with "relation" in hash keys
+        hashes.each do |hash|
+          hash.transform_keys! { |key| key == "relation_type" ? "relation" : key }
+        end
+
+        build_identifier_values(hashes)
+      end
+
+      def apply_label(label, value)
+        "<strong>#{label}:</strong> #{value}"
+      end
+
+      def build_identifier_values(hashes)
+        result = []
+        hashes.each do |hash|
+          hash.each do |key, value|
+            label = key.split('_').last
+            result << apply_label(label, value)
+          end
+        end
+
+        result
+      end
   end
 end
 
