3377e037b5d15e37145d61c5c47ea608cfbf4810
LaRita Robinson <larita@scientist.com>
2023-04-13 17:51:10 -0400

    WIP
    Working to add to OAI feed.

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -87,6 +87,7 @@ class SolrDocument
   attribute :collection_id, Solr::Array, solr_name('collection_id')
   attribute :ethos_access_rights, Solr::Array, solr_name('ethos_access_rights')
   attribute :record_level_file_version_declaration, Solr::String, solr_name('record_level_file_version_declaration')
+  attribute :open_access_determination, Solr::String, solr_name('open_access') 
 
   field_semantics.merge!(
     contributor: ['contributor_list_tesim', 'editor_list_tesim', 'funder_tesim'],
