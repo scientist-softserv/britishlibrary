33493aa04893d8f6e99e1e1202e46a8df9214ddc
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2023-04-26 22:49:53 +0100

    indentation fixes

--- app/services/hyrax/statistics/depositors/summary_decorator.rb
+++ app/services/hyrax/statistics/depositors/summary_decorator.rb
@@ -1,24 +1,22 @@
 # Override Hyrax 2.9.6 to stop error when trprting on deposits by since removed users
 #
 module Hyrax
-   module Statistics
-       module Depositors
-         module SummaryDecorator
-
-          def depositors
-            # step through the array by twos to get each pair
-            results.map do |key, deposits|
-              user = ::User.find_by_user_key(key)
-             # Don't freak out if user not found (they may have been removed)
-             if user
-               { key: key, deposits: deposits, user: user }
-             else
-               # Leave user undef and let the view work out what to do
-               { key: key, deposits: deposits }
+  module Statistics
+    module Depositors
+      module SummaryDecorator
+        def depositors
+          # step through the array by twos to get each pair
+          results.map do |key, deposits|
+            user = ::User.find_by_user_key(key)
+            # Don't freak out if user not found (they may have been removed)
+            if user
+              { key: key, deposits: deposits, user: user }
+            else
+              # Leave user undef and let the view work out what to do
+              { key: key, deposits: deposits }
             end
           end
         end
-
       end
     end
   end
