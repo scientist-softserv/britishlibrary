5e037cd52b586974c7b8bec688119c76b5a562bc
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2023-12-12 10:19:38 +0000

    Clamby (#496)
    * app user to run clamav as daemon
    
    * use clamby gem in preference to clamav gem
    
    * make sure clamav-daemon (clamd) is running on start up
    
    * switch interface for clamav from clamav gem to clamby (as per Hyc)
    
    * The Hyc::VirusScanner Module
    
    * tidy for rubocop
    
    * run clamd with sidekiq

--- config/initializers/clamav.rb
+++ config/initializers/clamav.rb
@@ -1,1 +1,19 @@
-ClamAV.instance.loaddb if defined? ClamAV
+# Pre Hyrax 3.5 we will overide use of clamav gem and use clamby
+# Inspired by https://github.com/UNC-Libraries/hy-c/commit/57c84bd0fdfb9ee8b00cc70194971c3fe9fea265#diff-8b7db4d5cc4b8f6dc8feb7030baa2478
+Hydra::Works.default_system_virus_scanner = Hyc::VirusScanner
+
+Clamby.configure({
+                     :check => false,
+                     :daemonize => true,
+                     :config_file => nil,
+                     :error_clamscan_missing => true,
+                     :error_clamscan_client_error => false,
+                     :error_file_missing => true,
+                     :error_file_virus => false,
+                     :fdpass => true,
+                     :stream => false,
+                     :output_level => 'medium', # one of 'off', 'low', 'medium', 'high'
+                     :executable_path_clamscan => 'clamscan',
+                     :executable_path_clamdscan => 'clamdscan',
+                     :executable_path_freshclam => 'freshclam',
+                 })
