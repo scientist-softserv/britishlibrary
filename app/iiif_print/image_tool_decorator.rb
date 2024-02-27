dec700636c7fd83f1a66fdaf0c2117e007244cef
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-02-27 10:47:46 +0000

    override im_identify and use -ping option

---
+++ app/iiif_print/image_tool_decorator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# OVERRIDE IIIF Print v1.0.0
+
+module IiifPrint
+  module ImageToolDecorator
+    # Add -ping option for efficiency
+    def im_identify
+      STDERR.puts "######################################################################"
+      STDERR.puts "#################### IN OVERRID IM_IDENTIFY ##########################"
+      STDERR.puts "######################################################################"
+      cmd = "identify -ping -format 'Geometry: %G\nDepth: %[bit-depth]\nColorspace: %[colorspace]\nAlpha: %A\nMIME type: %m\n' #{path}"
+      `#{cmd}`.lines
+    end
+ end
+end
+
+IiifPrint::ImageTool.prepend(IiifPrint::ImageToolDecorator)
