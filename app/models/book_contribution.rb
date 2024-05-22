f83cc25abddd64f6c8c8392aa05a4e63c94535eb
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-05-22 17:06:28 +0100

    Add PDF page limit for IiifPring splitter, PdfPage as pdf_split_child_model for BookContribution (#545)

--- app/models/book_contribution.rb
+++ app/models/book_contribution.rb
@@ -14,7 +14,7 @@ class BookContribution < ActiveFedora::Base
   # Adds behaviors for DataCite DOIs via hyrax-doi plugin.
   include Hyrax::DOI::DataCiteDOIBehavior
   include IiifPrint.model_configuration(
-    pdf_split_child_model: self
+    pdf_split_child_model: PdfPage
   )
 
   self.indexer = BookContributionIndexer
