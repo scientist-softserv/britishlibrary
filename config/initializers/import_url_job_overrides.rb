e36aca969cb3aee0121418658e1b8c0a78492b82
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-07-24 16:08:12 +0100

    remove rescue get error

--- config/initializers/import_url_job_overrides.rb
+++ config/initializers/import_url_job_overrides.rb
@@ -6,13 +6,13 @@ ImportUrlJob.class_eval do
     Rails.logger.debug("ImportUrlJob: Copying <#{uri}> to #{dir}")
 
     File.open(File.join(dir, filename), 'wb') do |f|
-      begin
+#      begin
         write_file(uri, f, headers)
         yield f
-      rescue StandardError => e
-        STDERR.puts("IMPORT URL ERROR: #{e}")
-        send_error(e.message)
-      end
+#      rescue StandardError => e
+#        STDERR.puts("IMPORT URL ERROR: #{e}")
+#        send_error(e.message)
+#      end
     end
     Rails.logger.debug("ImportUrlJob: Closing #{File.join(dir, filename)}")
   end
