3c8cc9e1ade3a63d97114045c03b8436964e450a
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-10-15 17:10:56 +0100

    don't use the S3 URL to name the file

--- lib/hydra/works/services/add_external_file_to_file_set_decorator.rb
+++ lib/hydra/works/services/add_external_file_to_file_set_decorator.rb
@@ -5,7 +5,10 @@ module Hydra
     module UpdaterDecorator
       def attach_attributes(external_file_url, filename = nil)
         current_file.content = StringIO.new('-') # anything but blank
-        current_file.original_name = filename
+        # filename will be the url as that is what ahppens in self.call
+        STDERR.puts "We will call this file: #{@file_set.label}... or maybe #{@file_set.file_name}.... or even #{file_set.label}"
+        current_file.original_name = @file_set.label
+#        current_file.original_name = filename
         current_file.mime_type = "message/external-body; access-type=URL; URL=\"#{external_file_url}\""
       end
     end
