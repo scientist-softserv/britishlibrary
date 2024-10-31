99d3742c5e64f63add996d7224d5b8550414b794
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-10-31 14:22:59 +0000

    add HRP and reactivate tate in inst menu (#571)
    * add HRP and reactivate tate in inst menu
    
    * fix sub-domain in helper

--- app/helpers/ubiquity/preselect_institution_helper.rb
+++ app/helpers/ubiquity/preselect_institution_helper.rb
@@ -7,14 +7,15 @@ module Ubiquity
       tenant_name = ubiquity_url_parser request.original_url
       institution_hash = {
                             'British Library' => ['sandbox', 'sandbox2', 'bl', 'bl-demo'],
-                            'MOLA' => ['sandbox2', 'mola', 'mola-demo'],
-                            'National Museums Scotland' => ['nms', 'nms-demo'],
                             'British Museum' => ['britishmuseum', 'britishmuseum-demo'],
-                            'Tate' => ['tate', 'tate-demo'],
+                            'Historic Royal Palaces' => ['historicroyalpalaces', 'historicroyalpalaces-demo'],
+                            'MOLA' => ['sandbox2', 'mola', 'mola-demo'],
+                            'National Library of Scotland' => ['nls', 'nls-demo'],
+                            'National Museums Scotland' => ['nms', 'nms-demo'],
+                            'National Trust' => ['nt', 'nt-demo'],
                             'Royal Botanic Gardens, Kew' => ['kew', 'kew-demo'],
                             'Science Museum Group' => ['sciencemuseumgroup', 'sciencemuseumgroup-demo'],
-                            'National Trust' => ['nt', 'nt-demo'],
-                            'National Library of Scotland' => ['nls', 'nls-demo'],
+                            'Tate' => ['tate', 'tate-demo'],
                             'The Alan Turing Institute' => ['ati', 'ati-demo'],
                             '' => ['nhs', 'nhs-demo']
                           }
